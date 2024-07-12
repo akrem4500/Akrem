ccf9173f2883ee63d5bb5adea48d08416f19bcc1
akrem4500 <133696548+akrem4500@users.noreply.github.com>
2024-07-12 22:03:12 +0100

    iplémentation de la methode de calcul de la moyenne

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ class Main {
     static sommeTableau(tab) {
       return tab.reduce((acc, val) => acc + val, 0);
     }
+
+     // Méthode pour calculer la moyenne d'un tableau d'entiers
+  static moyenneTableau(tab) {
+    if (tab.length === 0) return 0;
+    return this.sommeTableau(tab) / tab.length;
+  }
+
 }
