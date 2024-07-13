c420cc4a806dbeb8da96b6ea69a80d5e62e764b7
akrem4500 <133696548+akrem4500@users.noreply.github.com>
2024-07-13 09:42:04 +0100

    methode nombre premier

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ class Main {
   static estPair(n) {
     return n % 2 === 0;
   }
+// Méthode pour vérifier si un nombre est premier
+static estPremier(n) {
+    if (n <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
 }
+
+module.exports = Main;
